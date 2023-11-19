64890d34a50704e9b2cb56ef5258579667ecd93c
Ruiyot Abby <ruiyotabby@gmail.com>
2023-11-19 13:56:13 +0300

    Fix esling bug

---
+++ patientor-backend/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+	res.send('pong');
+});
+
+app.listen(PORT, () => console.log('listening on 3001'));
