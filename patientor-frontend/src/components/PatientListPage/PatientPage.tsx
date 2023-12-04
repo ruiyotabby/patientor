0ad871ff05ecd75e6dec3622fac954c24a29ef8b
Ruiyot Abby <ruiyotabby@gmail.com>
2023-12-04 21:02:41 +0300

    Allow fetching and displaying of diagnosis data

--- patientor-frontend/src/components/PatientListPage/PatientPage.tsx
+++ patientor-frontend/src/components/PatientListPage/PatientPage.tsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
-import { Patient } from '../../types';
+import { Diagnosis, Patient } from '../../types';
 import { Male, Female, Transgender } from '@mui/icons-material';
+import { useEffect, useState } from 'react';
+import diagnosisService from '../../services/diagnosis';
 
 interface Props {
 	patients: Patient[];
@@ -9,6 +11,11 @@ interface Props {
 const PatientPage = ({ patients }: Props) => {
 	const { id } = useParams();
 	const patient = patients.find((patient) => patient.id === id);
+	const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
+	useEffect(() => {
+		diagnosisService.getAll().then((response) => setDiagnoses(response));
+	}, []);
 
 	return (
 		<>
@@ -23,8 +30,21 @@ const PatientPage = ({ patients }: Props) => {
 				)}
 			</h3>
 			<div>ssh: {patient?.ssn}</div>
-
 			<div>occupation: {patient?.occupation}</div>
+			<h3>entries</h3>
+			{patient?.entries?.map((entry) => (
+				<div key={entry.id}>
+					{entry.date} {entry.description}
+					<ul>
+						{entry.diagnosisCodes?.map((code, id) => (
+							<li key={id}>
+								{code}{' '}
+								{diagnoses.find((diagnosis) => diagnosis.code === code)?.name}
+							</li>
+						))}
+					</ul>
+				</div>
+			))}
 		</>
 	);
 };
